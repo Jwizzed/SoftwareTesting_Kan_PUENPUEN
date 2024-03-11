28ba17849a72b9ef9e2d220f0e4d3f2dce047c19
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-03-11 20:57:26 +0700

    Change from unittest to pytest.

--- auction_test.py
+++ auction_test.py
@@ -1,65 +1,64 @@
-"""Tests of the Auction class.
-
-Author: your name
-"""
-import unittest
+import pytest
 from auction import Auction, AuctionError
 
 
-class AuctionTest(unittest.TestCase):
-    """Tests of the Auction class."""
+@pytest.fixture
+def auction():
+    """Create an auction before each test."""
+    return Auction("Test Auction")
 
-    def setUp(self):
-        """Create an auction before each test."""
-        self.auction = Auction("Test Auction")
 
-    def test_new_auction(self):
-        """a new auction has no bids and bidding is disabled."""
-        self.assertEqual(0, self.auction.best_bid())
-        self.assertEqual('no bids', self.auction.winner())
+def test_new_auction(auction):
+    """a new auction has no bids and bidding is disabled."""
+    assert auction.best_bid() == 0
+    assert auction.winner() == 'No Bids'
 
-    def test_lower_than_or_equal_to_zero_bit_price(self):
-        """The bit price shouldn't lower than or equal to 0."""
-        self.auction.start()
-        with self.assertRaises(ValueError):
-            self.auction.bid("Negative Bid", -1)
 
-        with self.assertRaises(ValueError):
-            self.auction.bid("Zero Bid", 0)
+def test_lower_than_or_equal_to_zero_bit_price(auction):
+    """The bit price shouldn't lower than or equal to 0."""
+    auction.start()
+    with pytest.raises(ValueError):
+        auction.bid("Negative Bid", -1)
 
-    def test_closed_auction_bid(self):
-        """Bidder shouldn't be able to bid the closed auction."""
-        self.auction.start()
-        self.auction.bid("User1", 100)
-        self.auction.stop()
-        with self.assertRaises(AuctionError):
-            self.auction.bid("Closed", self.auction.best_bid() +
-                             self.auction.increment)
+    with pytest.raises(ValueError):
+        auction.bid("Zero Bid", 0)
 
-    def test_bid_not_started_auction(self):
-        """Bidder shouldn't be able to bid the auction that not started yet."""
-        with self.assertRaises(AuctionError):
-            self.auction.bid("Not started", self.auction.best_bid() +
-                             self.auction.increment)
 
-    def test_blank_bidder(self):
-        """The bid method requires a non-blank bidder name."""
-        self.auction.start()
-        with self.assertRaises(ValueError):
-            self.auction.bid(" ", 100)
+def test_closed_auction_bid(auction):
+    """Bidder shouldn't be able to bid the closed auction."""
+    auction.start()
+    auction.bid("User1", 100)
+    auction.stop()
+    with pytest.raises(AuctionError):
+        auction.bid("Closed", auction.best_bid() + auction.increment)
 
-    def test_too_low_bid(self):
-        """The bid shouldn't below the increment."""
-        self.auction.start()
-        with self.assertRaises(AuctionError):
-            self.auction.bid("Bidder", 0.1)
 
-    def test_not_active_winner(self):
-        """The winner should show the winner name even inactive."""
-        self.auction = Auction("Test Auction 2", 100)
-        self.auction.start()
-        self.auction.bid("Bidder 1", 100.50)
-        self.auction.bid("Bidder 2", 200.50)
-        self.auction.bid("Bidder 3", 400.50)
-        self.auction.stop()
-        self.assertEqual("Bidder 3", self.auction.winner())
+def test_bid_not_started_auction(auction):
+    """Bidder shouldn't be able to bid the auction that not started yet."""
+    with pytest.raises(AuctionError):
+        auction.bid("Not started", auction.best_bid() + auction.increment)
+
+
+def test_blank_bidder(auction):
+    """The bid method requires a non-blank bidder name."""
+    auction.start()
+    with pytest.raises(ValueError):
+        auction.bid(" ", 100)
+
+
+def test_too_low_bid(auction):
+    """The bid shouldn't below the increment."""
+    auction.start()
+    with pytest.raises(AuctionError):
+        auction.bid("Bidder", 0.1)
+
+
+def test_not_active_winner(auction):
+    """The winner should show the winner name even inactive."""
+    auction = Auction("Test Auction 2", 100)
+    auction.start()
+    auction.bid("Bidder 1", 100.50)
+    auction.bid("Bidder 2", 200.50)
+    auction.bid("Bidder 3", 400.50)
+    auction.stop()
+    assert auction.winner() == "Bidder 3"
